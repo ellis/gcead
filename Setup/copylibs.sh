7356c8c85df060ccb1519796f50d4c13839c01fb
Ellis Whitehead <whiteh12@users.sourceforge.net>
2010-10-30 08:42:55 +0000

    Some website and installation modifications I made a couple weeks ago

--- Setup/copylibs.sh
+++ Setup/copylibs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-FILES=$(ldd ../release/GcEad | grep -e Qt -e libstd -e libgcc | sed -e 's#[^/]*\(/.*\) .*#\1#')
+FILES=$(ldd ../../GcEad-build-desktop/release/GcEad | grep -e Qt -e libstd -e libgcc | sed -e 's#[^/]*\(/.*\) .*#\1#')
+# Use release libraries instead of debug libraries
+FILES=$(echo "$FILES" | sed -e 's#/qt/lib/#/lib/#')
 mkdir -p libs
+rm -f libs/*
 rsync -v --copy-links $FILES libs
