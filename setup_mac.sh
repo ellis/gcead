5b5ecaec7ea23a8f87d69ddbce37002e1c503e91
Ellis Whitehead <ellis.whitehead@gmail.com>
2016-01-04 11:44:30 -0800

    Use windeployqt tool to gather dependencies for deployment

--- setup_mac.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p temp
-git clone https://github.com/libusb/libusb.git
-cd libusb
-git checkout -b temp v1.0.20
-
---
+++ setup_windows.bat
@@ -0,0 +1,2 @@
+set PATH=%PATH%;C:\Qt\Tools\mingw492_32\bin
+C:\Qt\5.5\mingw492_32\bin\windeployqt.exe --dir Installables\Windows\windeployqt ..\build-GcEad-Desktop_Qt_5_5_1_MinGW_32bit-Release\View\release\GcEad.exe
